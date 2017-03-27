499fff5c8bdb1c01786a77f83826868cc4e52049
Muslim Maliukov <Muslim.krum.85@gmail.com>
2017-03-28 00:49:45 +0300

    owl carousel section->servises dev

--- app/js/common.js
+++ app/js/common.js
@@ -8,6 +8,7 @@ $(function() {
 	    $spinner.fadeOut();
 	    $preloader.delay(350).fadeOut('slow');
 	});
+	//touch hamburger toggle function
     	var touch = $('.touch');
         var menu_mobile = $('.main-menu-mobile');
      
@@ -16,4 +17,34 @@ $(function() {
            menu_mobile.slideToggle();
            subMenuMobile.hide(150);
        });
+           //mobile buttton slide toggle function
+          		 function handler(event) {
+          		 	var target = $(event.target);
+          		 	if(target.is(".description-mobile button")) {
+          		 		target.next().slideToggle("slow");
+          		 	}
+          		 }
+       		$(".description-mobile button").click(handler); 
+
+       		//owl.carousel plugin 
+
+       		//section servises carousel
+       		$('.carousel-servises').owlCarousel({
+       			items: 4,
+       			autoplay: true,
+       			autoplayTimeout: 3000,
+       			smartSpeed:2000,
+       			navText: false,
+       			loop: true,
+       			smartSpeed: 700
+       		}); 
+       		//section revievs carousel
+       		  $('.owl-carousel').owlCarousel({
+       		  	items: 1,
+       		  	margin: 10,
+       		  	nav: true,
+       		  	navText: false,
+       		  	loop: true,
+       		  	smartSpeed: 700
+       		  });    
 });
